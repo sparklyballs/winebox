d9541903562301bdd728a808f21663eab3bb5c05
sparklyballs <sparklyballs@gmail.com>
2015-07-20 04:32:37 +0100

    Update start.sh

--- winebox/src/start.sh
+++ winebox/src/start.sh
@@ -5,11 +5,20 @@ if [[ $(cat /etc/timezone) != $TZ ]] ; then
   dpkg-reconfigure -f noninteractive tzdata
 fi
 
+mkdir -p /home/ubuntu/unraid
+mkdir -p /home/ubuntu/.config/pcmanfm/LXDE
+
+if [ ! -f "/home/ubuntu/.config/pcmanfm/LXDE/desktop-items-0.conf" ]
+cp  /root/desktop-items-0.conf /home/ubuntu/.config/pcmanfm/LXDE/desktop-items-0.conf
+chown ubuntu:users /home/ubuntu/.config/pcmanfm/LXDE/desktop-items-0.conf
+chmod 644 /home/ubuntu/.config/pcmanfm/LXDE/desktop-items-0.conf
+fi
+
 chown -R ubuntu:users /home/ubuntu/
+
 mkdir  /var/run/sshd
 mkdir  /root/.vnc
 /usr/bin/supervisord -c /root/supervisord.conf
 while [ 1 ]; do
 /bin/bash
 done
-
